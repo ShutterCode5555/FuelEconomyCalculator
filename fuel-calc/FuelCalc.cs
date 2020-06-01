2ef5f2584e07a49786aec9b0f7d31ab899fcfcd8
ShutterCode5555 <shuttercode5555@gmail.com>
2020-06-01 23:06:10 +0100

    removed 2x calc methods, created calc static class

---
+++ fuel-calc/FuelCalc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace fuel_calc
+{
+    public static class FuelCalc
+    {
+        public static double FuelEconomyCalc(double fuel, double distance, string filename, double multiplier)
+        {
+            double mpg = (distance / fuel) * multiplier;
+            StreamWriter writer;
+            writer = File.AppendText($"{filename}.txt");
+            writer.WriteLine(DateTime.Now);
+            writer.WriteLine(mpg);
+            writer.Close();
+            return mpg;
+        }
+        public static double FuelEconomyCalc(double fuel, double distance, string filename)
+        {
+            double mpg = distance / fuel;
+            StreamWriter writer;
+            writer = File.AppendText($"{filename}.txt");
+            writer.WriteLine(DateTime.Now);
+            writer.WriteLine(mpg);
+            writer.Close();
+            return mpg;
+        }
+    }
+}
