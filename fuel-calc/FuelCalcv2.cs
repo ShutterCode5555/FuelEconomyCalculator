53df53a66e3e490c18fd5a410d46d6d6542cbe3f
ShutterCode5555 <shuttercode5555@gmail.com>
2020-06-04 12:03:50 +0100

    wip branch, adding classes

---
+++ fuel-calc/FuelCalcv2.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+namespace fuel_calc
+{
+    class FuelCalcv2
+    {
+        private double dataEntry;
+        private bool hasParsedEntry;
+        private double result;
+        public double DataEntry
+        {
+            get { return dataEntry; }
+        }
+
+
+        public double Result
+        {
+            get { return result; }
+        }
+        public double Entry(double constantMin, double constantMax, string entryTypeWriteline, string verb)
+        {
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"Type {entryTypeWriteline} {verb}:");
+                string entryString = Console.ReadLine();
+                hasParsedEntry = Double.TryParse(entryString, out dataEntry);
+                if (!hasParsedEntry)
+                {
+                    Console.WriteLine("Invalid number entered, press ENTER to try again");
+                    Console.ReadLine();
+                    dataEntry = 0;
+                }
+            }
+            while (dataEntry < constantMin || dataEntry > constantMax);
+
+            return result;
+        }
+
+
+
+
+        public double Calculate(double input1, double input2, string filename, double multiplier = 1)
+        {
+            result = (input1 / input2) * multiplier;
+
+            return Result;
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+//    public double DataEntry(string EntryString)
+//  {
+//    bool hasParsedEntry = Double.TryParse(entryString, out userEntry);
+//    if (!hasParsedEntry)
+//   {
+//      Console.WriteLine("Invalid number entered, press ENTER to try again");
+//     Console.ReadLine();
+//    userEntry = 0;
+//  }
+//  return userEntry;
+// }
+
+
--- fuel-calc/Program.cs
+++ fuel-calc/Program.cs
@@ -35,72 +35,96 @@ namespace fuel_calc
                 int menuChoice;
                 do
                 {
-                Console.Clear();
-                Console.WriteLine($"Fuel Economy Calculator{nl}======================={nl}");
-                Console.WriteLine("[1] Calculate MPG - litres/miles");
-                Console.WriteLine("[2] Calculate MPG  - gallons(US or Imperial)/miles");
-                Console.WriteLine("[3] Calculate L/100km - litres/kilometres");
-                Console.WriteLine($"[4] Calculate KPL - litres/kilometres{nl}{nl}");
-                Console.WriteLine("[5] View History");
-                Console.WriteLine("[6] Instructions");
-                Console.WriteLine($"[7] Quit{nl}");
-                Console.WriteLine("Please type the corresponding number for desired menu option and press ENTER:");
-                string menuChoiceString = Console.ReadLine();
-                hasParsedMenuEntry = Int32.TryParse(menuChoiceString, out menuChoice);
+                    Console.Clear();
+                    Console.WriteLine($"Fuel Economy Calculator{nl}======================={nl}");
+                    Console.WriteLine("[1] Calculate MPG - litres/miles");
+                    Console.WriteLine("[2] Calculate MPG  - gallons(US or Imperial)/miles");
+                    Console.WriteLine("[3] Calculate L/100km - litres/kilometres");
+                    Console.WriteLine($"[4] Calculate KPL - litres/kilometres{nl}{nl}");
+                    Console.WriteLine("[5] View History");
+                    Console.WriteLine("[6] Instructions");
+                    Console.WriteLine($"[7] Quit{nl}");
+                    Console.WriteLine("Please type the corresponding number for desired menu option and press ENTER:");
+                    string menuChoiceString = Console.ReadLine();
+                    hasParsedMenuEntry = Int32.TryParse(menuChoiceString, out menuChoice);
                 }
                 while (!hasParsedMenuEntry);
-                    switch (menuChoice)
-                    {
-                        //MPG
-                        case 1:
-                            double outputCase1, fuelAmountCase1, distanceCase1, outputCase1a, outputResult;
-                            outputCase1 = CalcEntry(MIN_LITRES, MAX_LITRES, "litres", PUMPED);
-                            fuelAmountCase1 = outputCase1;
-                            outputCase1a = CalcEntry(MIN_MILES, MAX_MILES, "miles", TRAVELLED);
-                            distanceCase1 = outputCase1a;
-                            outputResult = FuelCalc.FuelEconomyCalc(fuelAmountCase1, distanceCase1, MPG_FILENAME, LITRES2GALLONS);
-                            Console.WriteLine($"Your MPG is: {outputResult}{nl}");
-                            Console.WriteLine("Press ENTER to continue");
-                            Console.ReadLine();
-                            break;
-                        //MPG (US/Imperial)
-                        case 2:
-                            double outputCase2, fuelAmountCase2, distanceCase2, outputCase2a, outputResult2;
-                            outputCase2 = CalcEntry(MIN_GALLONS, MAX_GALLONS, "gallons", PUMPED);
-                            fuelAmountCase2 = outputCase2;
-                            outputCase2a = CalcEntry(MIN_MILES, MAX_MILES, "miles", TRAVELLED);
-                            distanceCase2 = outputCase2a;
-                            outputResult2 = FuelCalc.FuelEconomyCalc(fuelAmountCase2, distanceCase2, MPG_FILENAME);
-                            Console.WriteLine($"Your MPG is: {outputResult2}{nl}");
-                            Console.WriteLine("Press ENTER to continue");
-                            Console.ReadLine();
-                            break;
-                        //LperKm100
-                        case 3:
-                            double outputCase3, fuelAmountCase3, distanceCase3, outputCase3a, outputResult3;
-                            outputCase3 = CalcEntry(MIN_LITRES, MAX_LITRES, "litres", PUMPED);
-                            fuelAmountCase3 = outputCase3;
-                            outputCase3a = CalcEntry(MIN_KM, MAX_KM, "km", TRAVELLED);
-                            distanceCase3 = outputCase3a;
-                            outputResult3 = FuelCalc.FuelEconomyCalc(distanceCase3, fuelAmountCase3, MPG_FILENAME, LP100KM); //method inputs distance+fuel reversed due to base calc diff
-                            Console.WriteLine($"Your L/100km is: {outputResult3}{nl}");
-                            Console.WriteLine("Press ENTER to continue");
-                            Console.ReadLine();
-                            break;
-                        //KPL
-                        case 4:
-                            double outputCase4, fuelAmountCase4, distanceCase4, outputCase4a, outputResult4;
-                            outputCase4 = CalcEntry(MIN_LITRES, MAX_LITRES, "litres", PUMPED);
-                            fuelAmountCase4 = outputCase4;
-                            outputCase4a = CalcEntry(MIN_KM, MAX_KM, "km", TRAVELLED);
-                            distanceCase4 = outputCase4a;
-                            outputResult4 = FuelCalc.FuelEconomyCalc(fuelAmountCase4, distanceCase4, MPG_FILENAME);
-                        Console.WriteLine($"Your KPL is: {outputResult4}{nl}");
-                            Console.WriteLine("Press ENTER to continue");
-                            Console.ReadLine();
-                            break;
-                        //History
-                        case 5:
+                switch (menuChoice)
+                {
+                    //MPG
+
+                    case 1:
+                        var fuelEntry = new FuelCalcv2();
+
+                        double fuelResult = fuelEntry.Entry(MIN_LITRES, MAX_LITRES, "litres", PUMPED);
+
+                        var distanceEntry = new FuelCalcv2();
+
+
+                        double distanceResult = distanceEntry.Entry(MIN_LITRES, MAX_LITRES, "litres", PUMPED);
+
+
+                        // fuelCalcv2.Calculate(fuelCalcv2.FuelEntry, fuelCalcv2.DistanceEntry, MPG_FILENAME, LITRES2GALLONS);
+                        double resultData = distanceEntry.Calculate(fuelResult, distanceResult, MPG_FILENAME, LITRES2GALLONS);
+
+                        Console.Clear();
+                        Console.WriteLine($"Your MPG is: {resultData}{nl}");
+
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+
+
+
+                        //    case 1:
+                        //      double outputCase1, fuelAmountCase1, distanceCase1, outputCase1a, outputResult;
+                        //       outputCase1 = CalcEntry(MIN_LITRES, MAX_LITRES, "litres", PUMPED);
+                        //       fuelAmountCase1 = outputCase1;
+                        //      outputCase1a = CalcEntry(MIN_MILES, MAX_MILES, "miles", TRAVELLED);
+                        //       distanceCase1 = outputCase1a;
+                        //       outputResult = fuelCalc.FuelEconomyCalc(fuelAmountCase1, distanceCase1, MPG_FILENAME, LITRES2GALLONS);
+                        //       Console.WriteLine($"Your MPG is: {outputResult}{nl}");
+                        //      Console.WriteLine("Press ENTER to continue");
+                        //      Console.ReadLine();
+                        break;
+                    //MPG (US/Imperial)
+                    case 2:
+                        // var fuelcalc = new FuelCalc();
+                        double outputCase2, fuelAmountCase2, distanceCase2, outputCase2a, outputResult2;
+                        outputCase2 = CalcEntry(MIN_GALLONS, MAX_GALLONS, "gallons", PUMPED);
+                        fuelAmountCase2 = outputCase2;
+                        outputCase2a = CalcEntry(MIN_MILES, MAX_MILES, "miles", TRAVELLED);
+                        distanceCase2 = outputCase2a;
+                        //      outputResult2 = fuelCalc.FuelEconomyCalc(fuelAmountCase2, distanceCase2, MPG_FILENAME);
+                        //     Console.WriteLine($"Your MPG is: {outputResult2}{nl}");
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+                    //LperKm100
+                    case 3:
+                        double outputCase3, fuelAmountCase3, distanceCase3, outputCase3a, outputResult3;
+                        outputCase3 = CalcEntry(MIN_LITRES, MAX_LITRES, "litres", PUMPED);
+                        fuelAmountCase3 = outputCase3;
+                        outputCase3a = CalcEntry(MIN_KM, MAX_KM, "km", TRAVELLED);
+                        distanceCase3 = outputCase3a;
+                        //    outputResult3 = fuelCalc.FuelEconomyCalc(distanceCase3, fuelAmountCase3, MPG_FILENAME, LP100KM); //method inputs distance+fuel reversed due to base calc diff
+                        //     Console.WriteLine($"Your L/100km is: {outputResult3}{nl}");
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+                    //KPL
+                    case 4:
+                        double outputCase4, fuelAmountCase4, distanceCase4, outputCase4a, outputResult4;
+                        outputCase4 = CalcEntry(MIN_LITRES, MAX_LITRES, "litres", PUMPED);
+                        fuelAmountCase4 = outputCase4;
+                        outputCase4a = CalcEntry(MIN_KM, MAX_KM, "km", TRAVELLED);
+                        distanceCase4 = outputCase4a;
+                        //    outputResult4 = fuelCalc.FuelEconomyCalc(fuelAmountCase4, distanceCase4, MPG_FILENAME);
+                        //   Console.WriteLine($"Your KPL is: {outputResult4}{nl}");
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+                    //History
+                    case 5:
                         int historyChoice;
                         string historyChoiceString;
                         bool historyQuit = false;
@@ -165,24 +189,24 @@ namespace fuel_calc
                             }
                         }
                         while (!historyQuit);
-                            break;
-                        case 6:
-                            Console.Clear();
-                            Console.WriteLine("Instructions");
-                            Console.WriteLine($"{nl}To use this program, you must first fill your car's{nl}fuel tank to the brim, then zero your trip counter. Then{nl}" +
-                                $"drive for a week and then refuel, making a note of the{nl}litres or gallons it took to refill the tank. Then after taking{nl}" +
-                                $"a note of the miles or km on the trip counter an zeroing the trip{nl}put the two sets of numbers into the calculator for the unit of{nl}" +
-                                $"measurement chosen.{nl}{nl}Each calculator has the units of measurement used for the calculation{nl}next to it's menu option on the main menu.{nl}{nl}");
-                            Console.WriteLine("Press ENTER to continue");
-                            Console.ReadLine();
-                            break;
-                        case 7:
-                            quit = true;
-                            break;
-                        default:
-                            Console.Clear();
-                            break;
-                    }
+                        break;
+                    case 6:
+                        Console.Clear();
+                        Console.WriteLine("Instructions");
+                        Console.WriteLine($"{nl}To use this program, you must first fill your car's{nl}fuel tank to the brim, then zero your trip counter. Then{nl}" +
+                            $"drive for a week and then refuel, making a note of the{nl}litres or gallons it took to refill the tank. Then after taking{nl}" +
+                            $"a note of the miles or km on the trip counter an zeroing the trip{nl}put the two sets of numbers into the calculator for the unit of{nl}" +
+                            $"measurement chosen.{nl}{nl}Each calculator has the units of measurement used for the calculation{nl}next to it's menu option on the main menu.{nl}{nl}");
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                        break;
+                    case 7:
+                        quit = true;
+                        break;
+                    default:
+                        Console.Clear();
+                        break;
+                }
             }
             while (!quit);
         }
@@ -201,11 +225,11 @@ namespace fuel_calc
         }
         static void HistoryClear(string filename)
         {
-                StreamWriter writer;
-                writer = new StreamWriter($"{filename}.txt");
-                writer.WriteLine(DateTime.Now);
-                writer.WriteLine("HISTORY CLEARED");
-                writer.Close();
+            StreamWriter writer;
+            writer = new StreamWriter($"{filename}.txt");
+            writer.WriteLine(DateTime.Now);
+            writer.WriteLine("HISTORY CLEARED");
+            writer.Close();
         }
         static double CalcEntry(double constantMin, double constantMax, string entryTypeWriteline, string verb)
         {
